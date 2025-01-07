f87d1ebe84cda45bb7d32b66f5be90a7f7bb1b5b
pavankalyan <114129248+pavanhoney630@users.noreply.github.com>
2025-01-07 23:09:11 +0530

    update

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -6,9 +6,10 @@ const jwt = require('jsonwebtoken');
 
 const signup = async (req, res) => {
 
-    const { username, email, password,confirmPassword } = req.body;
-    const { username, email, password, confirmPassword } = req.body;
->>>>>>> c0687879ddcbfe5fe9ca5b497ae6042b5825db98
+  const { username, email, password,confirmPassword } = req.body;
+
+   
+
   
     // Check if all fields are provided
     if (!username || !email || !password ) {
